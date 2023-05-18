095b2cace5ea6c5254f48bd8ef2f5266c5102bad
adz-brad <bradley.smith@arcticleaf.io>
2023-05-18 11:20:43 -0600

    add nav to root layout

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -1,14 +1,16 @@
 import '../styles/globals.css'
+import Navbar from '../components/nav/Navbar'
 
 export const metadata = {
   title: 'devJam',
   description: 'Next-Gen Web Development',
 }
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main>
           {children}
         </main>
